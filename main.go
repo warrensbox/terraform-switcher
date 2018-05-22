acb5999c85a70c1aa1233165609527dffd4fd784
warren-veerasingam <warren.veerasingam@gmail.com>
2018-05-21 19:03:55 -0500

    rm error

--- main.go
+++ main.go
@@ -30,7 +30,6 @@ import (
 	"github.com/manifoldco/promptui"
 	lib "github.com/warren-veerasingam/terraform-switcher/lib"
 )
-/Users/uveerum/Personal/go/src/github.com/warren-veerasingam/lib
 
 type tfVersionList struct {
 	tflist []string
