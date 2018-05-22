a7e3d4c6cdb0cb1f01c1294e9d34e2509ba257b5
warren-veerasingam <warren.veerasingam@gmail.com>
2018-05-22 10:58:16 -0500

    Complete files library testing

--- lib/files_test.go
+++ lib/files_test.go
@@ -1,8 +1,12 @@
 package lib_test
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"os/user"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/warren-veerasingam/terraform-switcher/lib"
@@ -87,4 +91,73 @@ func TestRemoveFiles(t *testing.T) {
 	}
 
 	cleanUp(installLocation)
+}
+
+// TestUnzip : Create a file, check file exist,
+// remove file, check file does not exist
+func TestUnzip(t *testing.T) {
+
+	installPath := "/.terraform.versions_test/"
+	absPath, _ := filepath.Abs("../test-data/test-data.zip")
+
+	fmt.Println(absPath)
+
+	usr, errCurr := user.Current()
+	if errCurr != nil {
+		log.Fatal(errCurr)
+	}
+	installLocation := usr.HomeDir + installPath
+
+	createDirIfNotExist(installLocation)
+
+	files, errUnzip := lib.Unzip(absPath, installLocation)
+
+	if errUnzip != nil {
+		fmt.Println("Unable to unzip zip file")
+		log.Fatal(errUnzip)
+		os.Exit(1)
+	}
+
+	tst := strings.Join(files, "")
+
+	if exist := checkFileExist(tst); exist {
+		t.Logf("File exist %v", tst)
+	} else {
+		t.Logf("File does not exist %v", tst)
+		t.Error("Missing file")
+	}
+
+	cleanUp(installLocation)
+}
+
+// TestCreateDirIfNotExist : Create a directory, check directory exist,
+func TestCreateDirIfNotExist(t *testing.T) {
+
+	installPath := "/.terraform.versions_test/"
+
+	usr, errCurr := user.Current()
+	if errCurr != nil {
+		log.Fatal(errCurr)
+	}
+	installLocation := usr.HomeDir + installPath
+
+	cleanUp(installLocation)
+
+	if _, err := os.Stat(installLocation); os.IsNotExist(err) {
+		t.Logf("Directory should not exist %v (expected)", installLocation)
+	} else {
+		t.Logf("Directory already exist %v (unexpected)", installLocation)
+		t.Error("Directory should not exist")
+	}
+
+	lib.CreateDirIfNotExist(installLocation)
+	t.Logf("Creating directory %v", installLocation)
+
+	if _, err := os.Stat(installLocation); err == nil {
+		t.Logf("Directory exist %v (expected)", installLocation)
+	} else {
+		t.Logf("Directory should exist %v (unexpected)", installLocation)
+		t.Error("Directory should exist")
+	}
+
 }
