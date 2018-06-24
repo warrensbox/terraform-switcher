5727c26c46146221b646986640d64588873565d8
Warren Veerasingam <warrenveerasingam@Warrens-MacBook-Pro-6.local>
2018-06-23 22:30:25 -0500

    remove vendor

--- docs/vendor/bundler/ruby/2.5.0/gems/ffi-1.9.23/ext/ffi_c/libffi-x86_64-linux/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/home/warrensbox/project/docs/vendor/bundler/ruby/2.5.0/gems/ffi-1.9.23/ext/ffi_c/libffi/src/x86/ffitarget.h
--- docs/vendor/bundler/ruby/2.5.0/gems/unicode-display_width-1.3.2/lib/unicode/display_width/index.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems/util'
-require_relative 'constants'
-
-module Unicode
-  module DisplayWidth
-    INDEX = Marshal.load(Gem::Util.gunzip(File.binread(INDEX_FILENAME)))
-  end
-end
