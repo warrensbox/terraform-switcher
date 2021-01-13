32f7569a0538e140bdcee22cb99907877a3ad7fc
warren.veerasingam <warren.veerasingam@gmail.com>
2021-01-13 14:14:54 +0800

    added option to download latest version

--- lib/list_versions.go
+++ lib/list_versions.go
@@ -17,7 +17,83 @@ type tfVersionList struct {
 //GetTFList :  Get the list of available terraform version given the hashicorp url
 func GetTFList(hashiURL string, listAll bool) ([]string, error) {
 
-	/* Get list of terraform versions from hashicorp releases */
+	result, error := GetTFURLBody(hashiURL)
+	if error != nil {
+		return nil, error
+	}
+
+	var tfVersionList tfVersionList
+
+	for i := range result {
+		// Getting versions from body; should return match /X.X.X/ where X is a number
+		r, _ := regexp.Compile(`\/(\d+\.\d+\.\d+)\/`)
+		if listAll {
+			// Getting versions from body; should return match /X.X.X-@/ where X is a number,@ is a word character between a-z or A-Z
+			r, _ = regexp.Compile(`\/(\d+\.\d+\.\d+)(-[a-zA-z]+\d*)?\/`)
+		}
+
+		if r.MatchString(result[i]) {
+			str := r.FindString(result[i])
+			trimstr := strings.Trim(str, "/") //remove "/" from /X.X.X/
+			tfVersionList.tflist = append(tfVersionList.tflist, trimstr)
+		}
+	}
+
+	return tfVersionList.tflist, nil
+
+}
+
+//GetTFLatestImplicit :  Get the latest terraform version given the hashicorp url
+func GetTFLatest(hashiURL string) (string, error) {
+
+	result, error := GetTFURLBody(hashiURL)
+	if error != nil {
+		return "", error
+	}
+	// Getting versions from body; should return match /X.X.X/ where X is a number
+	semver := `\/(\d+\.\d+\.\d+)\/`
+	r, _ := regexp.Compile(semver)
+	for i := range result {
+		if r.MatchString(result[i]) {
+			str := r.FindString(result[i])
+			trimstr := strings.Trim(str, "/") //remove "/" from /X.X.X/
+			return trimstr, nil
+		}
+	}
+
+	return "", nil
+}
+
+//GetTFLatestImplicit :  Get the latest terraform version given the hashicorp url
+func GetTFLatestImplicit(hashiURL string, preRelease bool, version string) (string, error) {
+
+	result, error := GetTFURLBody(hashiURL)
+	if error != nil {
+		return "", error
+	}
+	var semver string
+	if preRelease == true {
+		// Getting versions from body; should return match /X.X.X-@/ where X is a number,@ is a word character between a-z or A-Z
+		semver = fmt.Sprintf(`\/(%s{1}\.\d+\-[a-zA-z]+\d*)?\/`, version)
+	} else if preRelease == false {
+		semver = fmt.Sprintf(`\/(%s{1}\.\d+)\/`, version)
+	}
+	r, _ := regexp.Compile(semver)
+	for i := range result {
+		if r.MatchString(result[i]) {
+			str := r.FindString(result[i])
+			trimstr := strings.Trim(str, "/") //remove "/" from /X.X.X/
+			return trimstr, nil
+		}
+	}
+
+	return "", nil
+
+}
+
+//GetTFURLBody : Get list of terraform versions from hashicorp releases
+func GetTFURLBody(hashiURL string) ([]string, error) {
+
 	resp, errURL := http.Get(hashiURL)
 	if errURL != nil {
 		log.Printf("Error getting url: %v", errURL)
@@ -34,75 +110,7 @@ func GetTFList(hashiURL string, listAll bool) ([]string, error) {
 	bodyString := string(body)
 	result := strings.Split(bodyString, "\n")
 
-	var tfVersionList tfVersionList
-
-	for i := range result {
-		// Getting versions from body; should return match /X.X.X/ where X is a number
-		// Follow https://semver.org/spec/v2.0.0.html
-		r, _ := regexp.Compile(`\/(\d+\.\d+\.\d+)\/`)
-		if listAll {
-			// Getting versions from body; should return match /X.X.X-@/ where X is a number,@ is a word character between a-z or A-Z
-			// Follow https://semver.org/spec/v1.0.0-beta.html
-			// Check regular expression at https://rubular.com/r/ju3PxbaSBALpJB
-			r, _ = regexp.Compile(`\/(\d+\.\d+\.\d+)(-[a-zA-z]+\d*)?\/`)
-		}
-
-		if r.MatchString(result[i]) {
-			str := r.FindString(result[i])
-			trimstr := strings.Trim(str, "/") //remove "/" from /X.X.X/
-			tfVersionList.tflist = append(tfVersionList.tflist, trimstr)
-		}
-	}
-
-	return tfVersionList.tflist, nil
-
-}
-
-//GetTFLatest :  Get the latest terraform version given the hashicorp url
-func GetTFLatest(hashiURL string, listAll bool, version string) (string, error) {
-
-	/* Get list of terraform versions from hashicorp releases */
-	resp, errURL := http.Get(hashiURL)
-	if errURL != nil {
-		log.Printf("Error getting url: %v", errURL)
-		return "nil", errURL
-	}
-	defer resp.Body.Close()
-
-	body, errBody := ioutil.ReadAll(resp.Body)
-	if errBody != nil {
-		log.Printf("Error reading body: %v", errBody)
-		return "", errBody
-	}
-
-	bodyString := string(body)
-	result := strings.Split(bodyString, "\n")
-
-	var semver string
-	if version == "-" {
-		// Getting versions from body; should return match /X.X.X/ where X is a number
-		// Follow https://semver.org/spec/v2.0.0.html
-		semver = `\/(\d+\.\d+\.\d+)\/`
-	} else if listAll == true {
-		// Getting versions from body; should return match /X.X.X-@/ where X is a number,@ is a word character between a-z or A-Z
-		// Follow https://semver.org/spec/v1.0.0-beta.html
-		// Check regular expression at https://rubular.com/r/ju3PxbaSBALpJB
-		semver = fmt.Sprintf(`\/(%s{1}\.\d+\-[a-zA-z]+\d*)?\/`, version)
-	} else if listAll == false {
-		semver = fmt.Sprintf(`\/(%s{1}\.\d+)\/`, version)
-	}
-	r, _ := regexp.Compile(semver)
-	for i := range result {
-		if r.MatchString(result[i]) {
-			str := r.FindString(result[i])
-			trimstr := strings.Trim(str, "/") //remove "/" from /X.X.X/
-			fmt.Println(trimstr)
-			return trimstr, nil
-		}
-	}
-
-	return "", nil
-
+	return result, nil
 }
 
 //VersionExist : check if requested version exist
@@ -158,6 +166,21 @@ func ValidVersionFormat(version string) bool {
 	// Follow https://semver.org/spec/v1.0.0-beta.html
 	// Check regular expression at https://rubular.com/r/ju3PxbaSBALpJB
 	semverRegex := regexp.MustCompile(`^(\d+\.\d+\.\d+)(-[a-zA-z]+\d*)?$`)
+
+	return semverRegex.MatchString(version)
+}
+
+// ValidMinorVersionFormat : returns valid MINOR version format
+/* For example: 0.1 = valid
+// For example: a.1.2 = invalid
+// For example: 0.1.2 = invalid
+*/
+func ValidMinorVersionFormat(version string) bool {
+
+	// Getting versions from body; should return match /X.X.X-@/ where X is a number,@ is a word character between a-z or A-Z
+	// Follow https://semver.org/spec/v1.0.0-beta.html
+	// Check regular expression at https://rubular.com/r/ju3PxbaSBALpJB
+	semverRegex := regexp.MustCompile(`^(\d+\.\d+)`)
 
 	return semverRegex.MatchString(version)
 }
--- main.go
+++ main.go
@@ -30,73 +30,39 @@ import (
 	"github.com/Masterminds/semver"
 	"github.com/kiranjthomas/terraform-config-inspect/tfconfig"
 	"github.com/mitchellh/go-homedir"
-	"github.com/pborman/getopt"
-	"gopkg.in/alecthomas/kingpin.v2"
 
 	//	"github.com/hashicorp/terraform-config-inspect/tfconfig"
 
 	"github.com/manifoldco/promptui"
-	//"github.com/pborman/getopt"
+	"github.com/pborman/getopt"
 	"github.com/spf13/viper"
 
 	lib "github.com/warrensbox/terraform-switcher/lib"
 )
 
-var (
-	custBinPath *string
-	listAllFlag *bool
-	latest      *string
-	versionFlag *bool
-	helpFlag    *bool
-	arg         *string
-)
-
 const (
 	hashiURL      = "https://releases.hashicorp.com/terraform/"
 	defaultBin    = "/usr/local/bin/terraform" //default bin installation dir
+	defaultLatest = ""
 	tfvFilename   = ".terraform-version"
 	rcFilename    = ".tfswitchrc"
 	tomlFilename  = ".tfswitch.toml"
-	defaultlatest = ""
 )
-
-func init() {
-	const (
-		custBinPathDesc = "Custom binary path. For example: /Users/username/bin/terraform"
-		listAllFlagDesc = "List all versions of terraform - including beta and rc"
-		latestDesc      = "Get latest explicit version. For example: tfswitch --latest 0.13 will download 0.13.5 (latest). tfswitch --latest will download the most latest stable release"
-		argDesc         = "Pass version required"
-		helpDesc        = "Displays help message"
-	)
-
-	custBinPath = kingpin.Flag("bin", custBinPathDesc).Default(defaultBin).Short('b').String()
-	listAllFlag = kingpin.Flag("list-all", listAllFlagDesc).Short('l').Bool()
-	versionFlag = kingpin.Flag("version", listAllFlagDesc).Short('v').Bool()
-	//helpFlag = kingpin.Flag("h", helpDesc).Short('h').Bool()
-	latest = kingpin.Flag("latest", latestDesc).Default(defaultlatest).Short('a').String()
-	arg = kingpin.Arg("arg", "Supply terraform version as an argument. For example: tfswtich 0.14.0").String()
-}
 
 var version = "0.9.0\n"
 
 func main() {
+	custBinPath := getopt.StringLong("bin", 'b', defaultBin, "Custom binary path. Ex: /Users/username/bin/terraform")
+	listAllFlag := getopt.BoolLong("list-all", 'l', "List all versions of terraform - including beta and rc")
+	latestPre := getopt.StringLong("latest-pre", 'p', defaultLatest, "Latest pre-release implicit version. Ex: tfswitch --latest-pre 0.13 downloads 0.13.0-rc1 (latest)")
+	latestStable := getopt.StringLong("latest-stable", 's', defaultLatest, "Latest implicit version. Ex: tfswitch --latest 0.13 downloads 0.13.5 (latest)")
+	latestFlag := getopt.BoolLong("latest", 'u', "Get latest stable version")
+	versionFlag := getopt.BoolLong("version", 'v', "Displays the version of tfswitch")
+	helpFlag := getopt.BoolLong("help", 'h', "Displays help message")
+	_ = versionFlag
 
-	// custBinPath := getopt.StringLong("bin", 'b', defaultBin, "Custom binary path. For example: /Users/username/bin/terraform")
-	// listAllFlag := getopt.BoolLong("list-all", 'l', "List all versions of terraform - including beta and rc")
-	// latest := getopt.StringLong("latest", 'a', defaultlatest, "Custom binary path. For example: tfswitch --latest 0.13 will download 0.13.5 (latest). tfswitch --latest will download the most latest stable release")
-	// versionFlag := getopt.BoolLong("version", 'v', "Displays the version of tfswitch")
-	// helpFlag := getopt.BoolLong("help", 'h', "Displays help message")
-	// _ = versionFlag
-
-	kingpin.CommandLine.Interspersed(false)
-	kingpin.Parse()
-
-	//os.Exit(0)
-	/* check if arg is provided; set bool = true */
-	argProvided := false
-	if *arg != "" {
-		argProvided = true
-	}
+	getopt.Parse()
+	args := getopt.Args()
 
 	dir, err := os.Getwd() //get current directory
 	if err != nil {
@@ -110,18 +76,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	curr_tfvfile := dir + fmt.Sprintf("/%s", tfvFilename)             //settings for .terraform-version file in current directory (tfenv compatible)
-	curr_rcfile := dir + fmt.Sprintf("/%s", rcFilename)               //settings for .tfswitchrc file in current directory (backward compatible purpose)
-	curr_tomlconfigfile := dir + fmt.Sprintf("/%s", tomlFilename)     //settings for .tfswitch.toml file in current directory (option to specify bin directory)
-	home_tomlconfigfile := homedir + fmt.Sprintf("/%s", tomlFilename) //settings for .tfswitch.toml file in home directory (option to specify bin directory)
+	TFVersionFile := dir + fmt.Sprintf("/%s", tfvFilename)           //settings for .terraform-version file in current directory (tfenv compatible)
+	RCFile := dir + fmt.Sprintf("/%s", rcFilename)                   //settings for .tfswitchrc file in current directory (backward compatible purpose)
+	TOMLConfigFile := dir + fmt.Sprintf("/%s", tomlFilename)         //settings for .tfswitch.toml file in current directory (option to specify bin directory)
+	HomeTOMLConfigFile := homedir + fmt.Sprintf("/%s", tomlFilename) //settings for .tfswitch.toml file in home directory (option to specify bin directory)
 
 	switch {
 	case *versionFlag:
 		//if *versionFlag {
 		fmt.Printf("\nVersion: %v\n", version)
-	// case *helpFlag:
-	// 	//} else if *helpFlag {
-	// 	usageMessage()
+	case *helpFlag:
+		//} else if *helpFlag {
+		usageMessage()
 	/* Checks if the .tfswitch.toml file exist in home or current directory
 	 * This block checks to see if the tfswitch toml file is provided in the current path.
 	 * If the .tfswitch.toml file exist, it has a higher precedence than the .tfswitchrc file
@@ -129,11 +95,11 @@ func main() {
 	 * If you provide a custom binary path with the -b option, this will override the bin value in the toml file
 	 * If you provide a version on the command line, this will override the version value in the toml file
 	 */
-	case fileExists(curr_tomlconfigfile) || fileExists(home_tomlconfigfile):
+	case fileExists(TOMLConfigFile) || fileExists(HomeTOMLConfigFile):
 
 		version := ""
 		binPath := *custBinPath
-		if fileExists(curr_tomlconfigfile) { //read from toml from current directory
+		if fileExists(TOMLConfigFile) { //read from toml from current directory
 			version, binPath = getParamsTOML(binPath, dir)
 		} else { // else read from toml from home directory
 			version, binPath = getParamsTOML(binPath, homedir)
@@ -143,17 +109,25 @@ func main() {
 		case *listAllFlag:
 			listAll := true //set list all true - all versions including beta and rc will be displayed
 			installOption(listAll, &binPath)
-		case *arg != "":
-			installVersion(*arg, &binPath)
-		case fileExists(curr_rcfile) && argProvided:
+		case *latestPre != "":
+			preRelease := true
+			installLatestImplicitVersion(*latestPre, custBinPath, preRelease)
+		case *latestStable != "":
+			preRelease := false
+			installLatestImplicitVersion(*latestStable, custBinPath, preRelease)
+		case *latestFlag:
+			installLatestVersion(custBinPath)
+		case len(args) == 1:
+			installVersion(args[0], &binPath)
+		case fileExists(RCFile) && len(args) == 0:
 			readingFileMsg(rcFilename)
-			tfversion := retrieveFileContents(curr_rcfile)
+			tfversion := retrieveFileContents(RCFile)
 			installVersion(tfversion, &binPath)
-		case fileExists(curr_tfvfile) && argProvided:
+		case fileExists(TFVersionFile) && len(args) == 0:
 			readingFileMsg(tfvFilename)
-			tfversion := retrieveFileContents(curr_tfvfile)
+			tfversion := retrieveFileContents(TFVersionFile)
 			installVersion(tfversion, &binPath)
-		case checkTFModuleFileExist(dir) && argProvided:
+		case checkTFModuleFileExist(dir) && len(args) == 0:
 			installTFProvidedModule(dir, &binPath)
 		case version != "":
 			installVersion(version, &binPath)
@@ -162,32 +136,42 @@ func main() {
 			installOption(listAll, &binPath)
 		}
 
-	/* list all versions, //show all terraform version including betas and RCs*/
+	/* show all terraform version including betas and RCs*/
 	case *listAllFlag:
 		installWithListAll(custBinPath)
 
-	case *latest != "":
-		fmt.Println(*latest)
-		installLatestVersion(*latest, custBinPath)
+	/* latest pre-release implicit version. Ex: tfswitch --latest-pre 0.13 downloads 0.13.0-rc1 (latest) */
+	case *latestPre != "":
+		preRelease := true
+		installLatestImplicitVersion(*latestPre, custBinPath, preRelease)
+
+	/* latest implicit version. Ex: tfswitch --latest 0.13 downloads 0.13.5 (latest) */
+	case *latestStable != "":
+		preRelease := false
+		installLatestImplicitVersion(*latestStable, custBinPath, preRelease)
+
+	/* latest stable version */
+	case *latestFlag:
+		installLatestVersion(custBinPath)
 
 	/* version provided on command line as arg */
-	case argProvided:
-		installVersion(*arg, custBinPath)
+	case len(args) == 1:
+		installVersion(args[0], custBinPath)
 
 	/* provide an tfswitchrc file */
-	case fileExists(curr_rcfile) && argProvided:
+	case fileExists(RCFile) && len(args) == 0:
 		readingFileMsg(rcFilename)
-		tfversion := retrieveFileContents(curr_rcfile)
+		tfversion := retrieveFileContents(RCFile)
 		installVersion(tfversion, custBinPath)
 
 	/* if .terraform-version file found */
-	case fileExists(curr_tfvfile) && argProvided:
+	case fileExists(TFVersionFile) && len(args) == 0:
 		readingFileMsg(tfvFilename)
-		tfversion := retrieveFileContents(curr_tfvfile)
+		tfversion := retrieveFileContents(TFVersionFile)
 		installVersion(tfversion, custBinPath)
 
 	/* if versions.tf file found */
-	case checkTFModuleFileExist(dir) && argProvided:
+	case checkTFModuleFileExist(dir) && len(args) == 0:
 		installTFProvidedModule(dir, custBinPath)
 
 	// if no arg is provided
@@ -205,29 +189,20 @@ func installWithListAll(custBinPath *string) {
 	installOption(listAll, custBinPath)
 }
 
-// install latest with provided version as argument
-func installLatestVersion(arg string, custBinPath *string) {
-	//	if lib.ValidVersionFormat(arg) {
-	requestedVersion := arg
-	listAll := true
-	fmt.Println(requestedVersion)
-	os.Exit(0)
-	//set list all true - all versions including beta and rc will be displayed
-	tfversion, _ := lib.GetTFLatest(hashiURL, listAll, requestedVersion) //get list of versions
-	//exist := lib.VersionExist(requestedVersion, tflist)        //check if version exist before downloading it
-	fmt.Println(tfversion)
-	os.Exit(0)
-	// if exist {
+// install latest stable tf version
+func installLatestVersion(custBinPath *string) {
+	tfversion, _ := lib.GetTFLatest(hashiURL)
 	lib.Install(tfversion, *custBinPath)
-	// } else {
-	fmt.Println("The provided terraform version does not exist. Try `tfswitch -l` to see all available versions.")
-	// }
+}
 
-	// } else {
-	// 	printInvalidTFVersion()
-	// 	fmt.Println("Args must be a valid terraform version")
-	// 	usageMessage()
-	// }
+// install latest - argument (version) must be provided
+func installLatestImplicitVersion(requestedVersion string, custBinPath *string, preRelease bool) {
+	if lib.ValidMinorVersionFormat(requestedVersion) {
+		tfversion, _ := lib.GetTFLatestImplicit(hashiURL, preRelease, requestedVersion)
+		lib.Install(tfversion, *custBinPath)
+	} else {
+		printInvalidMinorTFVersion()
+	}
 }
 
 // install with provided version as argument
@@ -253,7 +228,12 @@ func installVersion(arg string, custBinPath *string) {
 
 // Print invalid TF version
 func printInvalidTFVersion() {
-	fmt.Println("Invalid terraform version format. Format should be #.#.# or #.#.#-@# where # is numbers and @ is word characters. For example, 0.11.7 and 0.11.9-beta1 are valid versions")
+	fmt.Println("Invalid terraform version format. Format should be #.#.# or #.#.#-@# where # are numbers and @ are word characters. For example, 0.11.7 and 0.11.9-beta1 are valid versions")
+}
+
+// Print invalid TF version
+func printInvalidMinorTFVersion() {
+	fmt.Println("Invalid minor terraform version format. Format should be #.# where # are numbers. For example, 0.11 is valid versions")
 }
 
 //retrive file content of regular file
@@ -361,7 +341,7 @@ func installOption(listAll bool, custBinPath *string) {
 	os.Exit(0)
 }
 
-// installation when
+// install when tf file is provided
 func installTFProvidedModule(dir string, custBinPath *string) {
 	tfversion := ""
 	module, _ := tfconfig.LoadModule(dir)
