a3168858f011555a56773fafccccd1b528122f4a
Johannes Brunswicker <johannes.brunswicker@gmail.com>
2024-04-21 10:18:28 +0200

    - show caller only if not info logging level

--- lib/logging.go
+++ lib/logging.go
@@ -6,14 +6,14 @@ import (
 	"os"
 )
 
-const loggingTemplate = "{{datetime}} {{level}} [{{caller}}] {{message}} {{data}} {{extra}}\n"
-
 var (
-	logger        *slog.Logger
-	NormalLogging = slog.Levels{slog.PanicLevel, slog.FatalLevel, slog.ErrorLevel, slog.WarnLevel, slog.InfoLevel}
-	NoticeLogging = slog.Levels{slog.PanicLevel, slog.FatalLevel, slog.ErrorLevel, slog.WarnLevel, slog.InfoLevel, slog.NoticeLevel}
-	DebugLogging  = slog.Levels{slog.PanicLevel, slog.FatalLevel, slog.ErrorLevel, slog.WarnLevel, slog.InfoLevel, slog.NoticeLevel, slog.DebugLevel}
-	TraceLogging  = slog.Levels{slog.PanicLevel, slog.FatalLevel, slog.ErrorLevel, slog.WarnLevel, slog.InfoLevel, slog.NoticeLevel, slog.DebugLevel, slog.TraceLevel}
+	loggingTemplateDebug = "{{datetime}} {{level}} [{{caller}}] {{message}} {{data}} {{extra}}\n"
+	loggingTemplate      = "{{datetime}} {{level}} {{message}} {{data}} {{extra}}\n"
+	logger               *slog.Logger
+	NormalLogging        = slog.Levels{slog.PanicLevel, slog.FatalLevel, slog.ErrorLevel, slog.WarnLevel, slog.InfoLevel}
+	NoticeLogging        = slog.Levels{slog.PanicLevel, slog.FatalLevel, slog.ErrorLevel, slog.WarnLevel, slog.InfoLevel, slog.NoticeLevel}
+	DebugLogging         = slog.Levels{slog.PanicLevel, slog.FatalLevel, slog.ErrorLevel, slog.WarnLevel, slog.InfoLevel, slog.NoticeLevel, slog.DebugLevel}
+	TraceLogging         = slog.Levels{slog.PanicLevel, slog.FatalLevel, slog.ErrorLevel, slog.WarnLevel, slog.InfoLevel, slog.NoticeLevel, slog.DebugLevel, slog.TraceLevel}
 )
 
 func InitLogger(logLevel string) *slog.Logger {
@@ -21,21 +21,24 @@ func InitLogger(logLevel string) *slog.Logger {
 	formatter.EnableColor = true
 	formatter.ColorTheme = slog.ColorTheme
 	formatter.TimeFormat = "15:04:05.000"
-	formatter.SetTemplate(loggingTemplate)
 
 	var h *handler.ConsoleHandler
 	switch logLevel {
 	case "TRACE":
 		h = handler.NewConsoleHandler(TraceLogging)
+		formatter.SetTemplate(loggingTemplateDebug)
 		break
 	case "DEBUG":
 		h = handler.NewConsoleHandler(DebugLogging)
+		formatter.SetTemplate(loggingTemplateDebug)
 		break
 	case "NOTICE":
 		h = handler.NewConsoleHandler(NoticeLogging)
+		formatter.SetTemplate(loggingTemplateDebug)
 		break
 	default:
 		h = handler.NewConsoleHandler(NormalLogging)
+		formatter.SetTemplate(loggingTemplate)
 	}
 
 	h.SetFormatter(formatter)
