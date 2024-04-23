87853c893ef84a8658d9e0794cb3a1232c5dd621
George L. Yermulnik <yz@yz.kiev.ua>
2024-04-23 16:55:50 +0300

    feat: Support multiple TF version constraints from module and improve logging (#362)
    * feat: Support multiple TF version constaints from module
    * Improve logging on reading TF version constraint (moved from #361)
    * Align with https://github.com/hashicorp/terraform-config-inspect/blob/master/main.go#L35C2-L37
    * Update `test-data/integration-tests/test_versiontf/version.tf` with multiple TF version constraints
    * Drop `required_providers` from `test-data/integration-tests/test_versiontf/version.tf` as `tfswitch` does not handle TF providers
    * Modified function to return an error in case of non matching constraints
    * Added testcase for non matching constraints
    * Moved failing testcase to skip-integration tests
    
    ---------
    
    Co-authored-by: Johannes Brunswicker <johannes.brunswicker@crownpeak.com>
    Co-authored-by: Hugh Wells <hugh@crablab.co.uk>

--- lib/param_parsing/versiontf_test.go
+++ lib/param_parsing/versiontf_test.go
@@ -4,20 +4,37 @@ import (
 	"fmt"
 	"github.com/hashicorp/go-version"
 	"github.com/warrensbox/terraform-switcher/lib"
+	"strings"
 	"testing"
 )
 
-func TestGetVersionFromVersionsTF(t *testing.T) {
+func TestGetVersionFromVersionsTF_matches_version(t *testing.T) {
 	logger = lib.InitLogger("DEBUG")
 	var params Params
 	params = initParams(params)
 	params.ChDirPath = "../../test-data/integration-tests/test_versiontf"
 	params, _ = GetVersionFromVersionsTF(params)
-	v1, _ := version.NewVersion("1.0.0")
-	v2, _ := version.NewVersion("2.0.0")
+	v1, _ := version.NewVersion("1.0.5")
 	actualVersion, _ := version.NewVersion(params.Version)
-	if !actualVersion.GreaterThanOrEqual(v1) || !actualVersion.LessThan(v2) {
-		t.Error("Determined version is not between 1.0.0 and 2.0.0")
+	if !actualVersion.Equal(v1) {
+		t.Error("Determined version is not 1.0.5")
+	}
+}
+
+func TestGetVersionFromVersionsTF_non_matching_constraints(t *testing.T) {
+	logger = lib.InitLogger("DEBUG")
+	var params Params
+	params = initParams(params)
+	params.ChDirPath = "../../test-data/skip-integration-tests/test_versiontf_non_matching_constraints"
+	params, err := GetVersionFromVersionsTF(params)
+	if err == nil {
+		t.Error("Expected error but got nil")
+	} else {
+		expected := "exact constraint ("
+		expected2 := ") cannot be combined with other conditions"
+		if !strings.Contains(fmt.Sprint(err), expected) || !strings.Contains(fmt.Sprint(err), expected2) {
+			t.Errorf("Expected error string containing %q and %q. Got %q", expected, expected2, err)
+		}
 	}
 }
 
--- test-data/integration-tests/test_versiontf/version.tf
+++ test-data/integration-tests/test_versiontf/version.tf
@@ -1,8 +1,7 @@
 terraform {
   required_version = "~> 1.0.0"
+}
 
-  required_providers {
-    aws        = ">= 2.52.0"
-    kubernetes = ">= 1.11.1"
-  }
+terraform {
+  required_version = "<= 1.0.5"
 }
