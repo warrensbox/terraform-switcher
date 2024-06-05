c3be17adcd62d236b2d025a6f6f8e3a6f62ccca7
Matthew John <matthew@dockstudios.co.uk>
2024-06-05 06:40:01 +0100

    Add additional tests for DownloadProductFromURL to test some failure cases

--- lib/download_test.go
+++ lib/download_test.go
@@ -12,6 +12,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strings"
 	"sync"
@@ -101,45 +102,69 @@ func TestDownloadFromURL_Valid(t *testing.T) {
 	}
 }
 
-// TestDownloadProductFromURL : Test DownloadProductFromURL
-func TestDownloadProductFromURL(t *testing.T) {
+type DownloadProductTestConfig struct {
+	GpgFingerprint      string
+	ZipFileContent      []byte
+	ZipFileChecksum     string
+	ChecksumFileContent string
+	PublicKey           string
+}
+
+func setupTestDownloadServer(t *testing.T, downloadProductTestConfig *DownloadProductTestConfig) *httptest.Server {
 	logger = InitLogger("DEBUG")
 	gpgKey, err := crypto.GenerateKey("TestProductSign", "example@localhost.com", "RSA", 1024)
 	if err != nil {
 		t.Fatal(err)
 	}
-	gpgFingerprint := gpgKey.GetFingerprint()[0:8]
-
-	zipFileBuffer := new(bytes.Buffer)
-	zipWriter := zip.NewWriter(zipFileBuffer)
-
-	mainExecutableBytes := []byte("This is the main executable")
-	zipFileContentWriter, err := zipWriter.Create("myprod")
-	if err != nil {
-		t.Fatal(err)
+	if downloadProductTestConfig.GpgFingerprint == "" {
+		downloadProductTestConfig.GpgFingerprint = gpgKey.GetFingerprint()[0:8]
 	}
-	zipFileContentWriter.Write(mainExecutableBytes)
-	zipWriter.Flush()
-	zipWriter.Close()
-	zipFileBytes := zipFileBuffer.Bytes()
 
-	publicKey, err := gpgKey.GetArmoredPublicKey()
-	if err != nil {
-		t.Fatal(err)
+	if len(downloadProductTestConfig.ZipFileContent) == 0 {
+		zipFileBuffer := new(bytes.Buffer)
+		zipWriter := zip.NewWriter(zipFileBuffer)
+
+		executableBytes := []byte("This is the main executable")
+		zipFileContentWriter, err := zipWriter.Create("myprod")
+		if err != nil {
+			t.Fatal(err)
+		}
+		zipFileContentWriter.Write(executableBytes)
+		zipWriter.Flush()
+		zipWriter.Close()
+		downloadProductTestConfig.ZipFileContent = zipFileBuffer.Bytes()
+	}
+
+	if downloadProductTestConfig.PublicKey == "" {
+		downloadProductTestConfig.PublicKey, err = gpgKey.GetArmoredPublicKey()
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
 	// Calculate SHA256 sum of ZIP file
-	sha256HashWriter := sha256.New()
-	if _, err = io.Copy(sha256HashWriter, zipFileBuffer); err != nil {
-		t.Fatal(err)
+	if downloadProductTestConfig.ZipFileChecksum == "" {
+		sha256HashWriter := sha256.New()
+		zipFileReadBuffer := new(bytes.Buffer)
+		_, err := zipFileReadBuffer.Write(downloadProductTestConfig.ZipFileContent)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err = io.Copy(sha256HashWriter, zipFileReadBuffer); err != nil {
+			t.Fatal(err)
+		}
+		sha256Hash := sha256HashWriter.Sum(nil)
+		downloadProductTestConfig.ZipFileChecksum = hex.EncodeToString(sha256Hash)
 	}
-	sha256Hash := sha256HashWriter.Sum(nil)
 
 	// Create checksum file
-	checksumFileContent := hex.EncodeToString(sha256Hash) + "  " + "my_product_download_2.1.0_linux_amd64.zip"
+	if downloadProductTestConfig.ChecksumFileContent == "" {
+		downloadProductTestConfig.ChecksumFileContent = downloadProductTestConfig.ZipFileChecksum + "  " + "my_product_download_2.1.0_linux_amd64.zip"
+	}
 
 	// Create signature of checksum file
-	binMessage := crypto.NewPlainMessageFromFile([]byte(checksumFileContent), "my_product_download_2.1.0_SHA256SUMS", uint32(crypto.GetUnixTime()))
+	binMessage := crypto.NewPlainMessageFromFile([]byte(downloadProductTestConfig.ChecksumFileContent), "my_product_download_2.1.0_SHA256SUMS", uint32(crypto.GetUnixTime()))
 	keyRing, err := crypto.NewKeyRing(gpgKey)
 	if err != nil {
 		t.Fatal(err)
@@ -156,16 +181,16 @@ func TestDownloadProductFromURL(t *testing.T) {
 		case "/testproduct/gpg-key.txt":
 			w.Header().Set("Content-Type", "text/html")
 			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(publicKey))
+			w.Write([]byte(downloadProductTestConfig.PublicKey))
 		case "/productdownload/2.1.0/my_product_download_2.1.0_linux_amd64.zip":
 			w.Header().Set("Content-Type", "application/zip")
 			w.WriteHeader(http.StatusOK)
-			w.Write(zipFileBytes)
+			w.Write(downloadProductTestConfig.ZipFileContent)
 		case "/productdownload/2.1.0/my_product_download_2.1.0_SHA256SUMS":
 			w.Header().Set("Content-Type", "text/plain")
 			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(checksumFileContent))
-		case "/productdownload/2.1.0/my_product_download_2.1.0_SHA256SUMS." + gpgFingerprint + ".sig":
+			w.Write([]byte(downloadProductTestConfig.ChecksumFileContent))
+		case "/productdownload/2.1.0/my_product_download_2.1.0_SHA256SUMS." + downloadProductTestConfig.GpgFingerprint + ".sig":
 			w.Header().Set("Content-Type", "text/plain")
 			w.WriteHeader(http.StatusOK)
 			w.Write(signatureObj.GetBinary())
@@ -173,6 +198,14 @@ func TestDownloadProductFromURL(t *testing.T) {
 			http.NotFoundHandler().ServeHTTP(w, r)
 		}
 	}))
+	return mockServer
+}
+
+// TestDownloadProductFromURL : Test DownloadProductFromURL
+func TestDownloadProductFromURL(t *testing.T) {
+	downloadProductTestConfig := DownloadProductTestConfig{}
+	mockServer := setupTestDownloadServer(t, &downloadProductTestConfig)
+	defer mockServer.Close()
 
 	// Create mock product
 	mockProduct := TerraformProduct{
@@ -183,7 +216,7 @@ func TestDownloadProductFromURL(t *testing.T) {
 			VersionPrefix:  "myprod_",
 			ExecutableName: "myprod",
 			ArchivePrefix:  "my_product_download_",
-			PublicKeyId:    gpgFingerprint,
+			PublicKeyId:    downloadProductTestConfig.GpgFingerprint,
 			PublicKeyUrl:   mockServer.URL + "/testproduct/gpg-key.txt",
 		},
 	}
@@ -201,5 +234,152 @@ func TestDownloadProductFromURL(t *testing.T) {
 	}
 	if expectedZipPath := filepath.Join(tempDir, "my_product_download_2.1.0_linux_amd64.zip"); zipFilePath != expectedZipPath {
 		t.Errorf("Returned zipFile not expected path. Expected: %q, actual: %q", expectedZipPath, zipFilePath)
+	}
+}
+
+// TestDownloadProductFromURL_invalid_checksum : Test DownloadProductFromURL with invalid zipfile checksum
+func TestDownloadProductFromURL_invalid_checksum(t *testing.T) {
+	downloadProductTestConfig := DownloadProductTestConfig{
+		ZipFileChecksum: "abcdef",
+	}
+	mockServer := setupTestDownloadServer(t, &downloadProductTestConfig)
+	defer mockServer.Close()
+
+	// Create mock product
+	mockProduct := TerraformProduct{
+		ProductDetails{
+			ID:             "myproduct",
+			Name:           "Mock Product",
+			DefaultMirror:  mockServer.URL + "/productdownload",
+			VersionPrefix:  "myprod_",
+			ExecutableName: "myprod",
+			ArchivePrefix:  "my_product_download_",
+			PublicKeyId:    downloadProductTestConfig.GpgFingerprint,
+			PublicKeyUrl:   mockServer.URL + "/testproduct/gpg-key.txt",
+		},
+	}
+
+	// Create temp location
+	tempDir, err := os.MkdirTemp("", "addRecentVersion")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	_, err = DownloadProductFromURL(mockProduct, tempDir, mockProduct.GetArtifactUrl(mockServer.URL+"/productdownload", "2.1.0"), "2.1.0", mockProduct.GetArchivePrefix(), "linux", "amd64")
+	if err == nil {
+		t.Fatal("DownloadProductFromURL did not throw error")
+	} else if expectedError := "Checksums did not match"; !strings.Contains(err.Error(), expectedError) {
+		t.Fatalf("DownloadProductFromURL did not throw expected error. Expected: %q, Actual: %q", expectedError, err)
+	}
+}
+
+// TestDownloadProductFromURL_zip_file_not_present_in_checksum : Test DownloadProductFromURL with zipfile not present in checksum
+func TestDownloadProductFromURL_zip_file_not_present_in_checksum(t *testing.T) {
+	downloadProductTestConfig := DownloadProductTestConfig{
+		ChecksumFileContent: "e64d27cf0fd05eaa0deab98756f4d533d14d467a7198b82a885be260e1ec4885  doesnotexist.txt",
+	}
+	mockServer := setupTestDownloadServer(t, &downloadProductTestConfig)
+	defer mockServer.Close()
+
+	// Create mock product
+	mockProduct := TerraformProduct{
+		ProductDetails{
+			ID:             "myproduct",
+			Name:           "Mock Product",
+			DefaultMirror:  mockServer.URL + "/productdownload",
+			VersionPrefix:  "myprod_",
+			ExecutableName: "myprod",
+			ArchivePrefix:  "my_product_download_",
+			PublicKeyId:    downloadProductTestConfig.GpgFingerprint,
+			PublicKeyUrl:   mockServer.URL + "/testproduct/gpg-key.txt",
+		},
+	}
+
+	// Create temp location
+	tempDir, err := os.MkdirTemp("", "addRecentVersion")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	_, err = DownloadProductFromURL(mockProduct, tempDir, mockProduct.GetArtifactUrl(mockServer.URL+"/productdownload", "2.1.0"), "2.1.0", mockProduct.GetArchivePrefix(), "linux", "amd64")
+	if err == nil {
+		t.Fatal("DownloadProductFromURL did not throw error")
+	} else if expectedError := "Checksums did not match"; !strings.Contains(err.Error(), expectedError) {
+		t.Fatalf("DownloadProductFromURL did not throw expected error. Expected: %q, Actual: %q", expectedError, err)
+	}
+}
+
+// TestDownloadProductFromURL_unable_to_download_public_key : Test DownloadProductFromURL unable to download public key
+func TestDownloadProductFromURL_unable_to_download_public_key(t *testing.T) {
+	downloadProductTestConfig := DownloadProductTestConfig{}
+	mockServer := setupTestDownloadServer(t, &downloadProductTestConfig)
+	defer mockServer.Close()
+
+	// Create mock product
+	mockProduct := TerraformProduct{
+		ProductDetails{
+			ID:             "myproduct",
+			Name:           "Mock Product",
+			DefaultMirror:  mockServer.URL + "/productdownload",
+			VersionPrefix:  "myprod_",
+			ExecutableName: "myprod",
+			ArchivePrefix:  "my_product_download_",
+			PublicKeyId:    downloadProductTestConfig.GpgFingerprint,
+			PublicKeyUrl:   mockServer.URL + "/testproduct/invalid-public-key",
+		},
+	}
+
+	// Create temp location
+	tempDir, err := os.MkdirTemp("", "addRecentVersion")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	_, err = DownloadProductFromURL(mockProduct, tempDir, mockProduct.GetArtifactUrl(mockServer.URL+"/productdownload", "2.1.0"), "2.1.0", mockProduct.GetArchivePrefix(), "linux", "amd64")
+	errorMatch := regexp.MustCompile("Unable to download from .*/testproduct/invalid-public-key")
+	if err == nil {
+		t.Fatal("DownloadProductFromURL did not throw error")
+	} else if !errorMatch.MatchString(err.Error()) {
+		t.Fatalf("DownloadProductFromURL did not throw expected error. Expected: %q, Actual: %q", errorMatch, err)
+	}
+}
+
+// TestDownloadProductFromURL_invalid_public_key : Test DownloadProductFromURL with invalid public key content
+func TestDownloadProductFromURL_invalid_public_key(t *testing.T) {
+	downloadProductTestConfig := DownloadProductTestConfig{
+		PublicKey: "thisisinvalid",
+	}
+	mockServer := setupTestDownloadServer(t, &downloadProductTestConfig)
+	defer mockServer.Close()
+
+	// Create mock product
+	mockProduct := TerraformProduct{
+		ProductDetails{
+			ID:             "myproduct",
+			Name:           "Mock Product",
+			DefaultMirror:  mockServer.URL + "/productdownload",
+			VersionPrefix:  "myprod_",
+			ExecutableName: "myprod",
+			ArchivePrefix:  "my_product_download_",
+			PublicKeyId:    downloadProductTestConfig.GpgFingerprint,
+			PublicKeyUrl:   mockServer.URL + "/testproduct/gpg-key.txt",
+		},
+	}
+
+	// Create temp location
+	tempDir, err := os.MkdirTemp("", "addRecentVersion")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	_, err = DownloadProductFromURL(mockProduct, tempDir, mockProduct.GetArtifactUrl(mockServer.URL+"/productdownload", "2.1.0"), "2.1.0", mockProduct.GetArchivePrefix(), "linux", "amd64")
+	if err == nil {
+		t.Fatal("DownloadProductFromURL did not throw error")
+	} else if expectedError := "Signature of checksum file could not be verified"; !strings.Contains(err.Error(), expectedError) {
+		t.Fatalf("DownloadProductFromURL did not throw expected error. Expected: %q, Actual: %q", expectedError, err)
 	}
 }
