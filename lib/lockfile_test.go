80256a35bb4b2e340c7a5ea8de579ed76a87fba1
George L. Yermulnik <yz@yz.kiev.ua>
2025-03-01 22:01:41 +0200

    Add test: `lib/lockfile_test.go`

---
+++ lib/lockfile_test.go
@@ -0,0 +1,25 @@
+package lib
+
+import (
+	"testing"
+)
+
+// Test Locking
+func TestLocking(t *testing.T) {
+	var lockFile string = "../test-data/lockfile.lock"
+
+	// Acquire lock
+	if lockAcquireErr := acquireLock(lockFile, 1, 1); lockAcquireErr == nil {
+		t.Logf("Lock acquired successfully: %s", lockFile)
+
+		// Release lock
+		releaseLock(lockFile)
+		if CheckFileExist(lockFile) {
+			t.Errorf("Failed to release lock: %s", lockFile)
+		} else {
+			t.Logf("Lock released successfully: %s", lockFile)
+		}
+	} else {
+		t.Errorf("Failed to acquire lock: %s", lockFile)
+	}
+}
