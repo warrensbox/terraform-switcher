867285287e0f71e94b1eef43a88e2db8abfd4050
George L. Yermulnik <yz@yz.kiev.ua>
2025-03-02 18:06:08 +0200

    Adjust test

--- lib/lockfile_test.go
+++ lib/lockfile_test.go
@@ -7,19 +7,21 @@ import (
 // Test Locking
 func TestLocking(t *testing.T) {
 	var lockFile string = "lockfile.lock"
+	tmpDir := t.TempDir()
+	lockFilePath := os.path.Join(tmpDir, lockFile)
 
 	// Acquire lock
-	if lockAcquireErr := acquireLock(lockFile, 1, 1); lockAcquireErr == nil {
-		t.Logf("Lock acquired successfully: %s", lockFile)
+	if lockedFile, err := acquireLock(lockFilePath, 1, 1); err == nil {
+		t.Logf("Lock acquired successfully: %s", err)
 
 		// Release lock
-		releaseLock(lockFile)
-		if CheckFileExist(lockFile) {
-			t.Errorf("Failed to release lock: %s", lockFile)
+		releaseLock(lockFilePath)
+		if CheckFileExist(lockFilePath) {
+			t.Errorf("Failed to release lock: %s", lockFilePath)
 		} else {
-			t.Logf("Lock released successfully: %s", lockFile)
+			t.Logf("Lock released successfully: %s", lockFilePath)
 		}
 	} else {
-		t.Errorf("Failed to acquire lock: %s", lockFile)
+		t.Errorf("Failed to acquire lock: %s", lockFilePath)
 	}
 }
