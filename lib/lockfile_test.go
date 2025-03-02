c3ef2e3003331b63962bf32d45bc56602e8fc7fa
George L. Yermulnik <yz@yz.kiev.ua>
2025-03-02 18:54:35 +0200

    Expand test as suggested in PR comments

--- lib/lockfile_test.go
+++ lib/lockfile_test.go
@@ -8,12 +8,21 @@ import (
 // Test Locking
 func TestLocking(t *testing.T) {
 	var lockFile string = ".tfswitch.lock"
-	tmpDir := t.TempDir()
-	lockFilePath := filepath.Join(tmpDir, lockFile)
+	lockFilePath := filepath.Join(t.TempDir(), lockFile)
+
+	t.Logf("Testing lock acquirement: %s", lockFilePath)
 
 	// Acquire lock
 	if lockedFile, err := acquireLock(lockFilePath, 1, 1); err == nil {
-		t.Logf("Lock acquired successfully: %s", err)
+		t.Logf("Lock acquired successfully: %s", lockFilePath)
+
+		// Concurrent lock
+		t.Logf("Testing concurrent lock acquirement: %s", lockFilePath)
+		if _, err := acquireLock(lockFilePath, 1, 1); err == nil {
+			t.Errorf("Concurrent lock acquired successfully: %s. This is NOT expected!", lockFilePath)
+		} else {
+			t.Logf("Concurrent lock failed: %s. This is expected.", lockFilePath)
+		}
 
 		// Release lock
 		releaseLock(lockFilePath, lockedFile)
@@ -21,6 +30,11 @@ func TestLocking(t *testing.T) {
 			t.Errorf("Failed to release lock: %s", lockFilePath)
 		} else {
 			t.Logf("Lock released successfully: %s", lockFilePath)
+		}
+
+		// Check lock was cleaned up
+		if exist := CheckFileExist(lockFile); exist {
+			t.Errorf("Lock %s still exists. This is NOT expected!", lockFilePath)
 		}
 	} else {
 		t.Errorf("Failed to acquire lock: %s", lockFilePath)
