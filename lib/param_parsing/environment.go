21219a1e81045fc9b16bd7daf3416b5a40083dd9
George L. Yermulnik <yz@yz.kiev.ua>
2025-03-16 17:46:52 +0200

    `lib/param_parsing/environment.go`: re-factor `GetParamsFromEnvironment()` for simplicity

--- lib/param_parsing/environment.go
+++ lib/param_parsing/environment.go
@@ -1,31 +1,56 @@
 package param_parsing
 
-import "os"
+import (
+	"os"
+	"reflect"
+)
 
 func GetParamsFromEnvironment(params Params) Params {
-	if envVar := os.Getenv("TF_ARCH"); envVar != "" {
-		params.Arch = envVar
-		logger.Debugf("Using architecture from environment variable \"TF_ARCH\": %q", envVar)
+	type envVar struct {
+		name        string
+		param       string
+		description string
 	}
-	if envVar := os.Getenv("TF_VERSION"); envVar != "" {
-		params.Version = envVar
-		logger.Debugf("Using version from environment variable \"TF_VERSION\": %q", envVar)
+	envVars := []envVar{
+		{name: "TF_ARCH", param: "Arch", description: "architecture"},
+		{name: "TF_BINARY_PATH", param: "CustomBinaryPath", description: "custom binary path"},
+		{name: "TF_DEFAULT_VERSION", param: "DefaultVersion", description: "default version"},
+		{name: "TF_INSTALL_PATH", param: "InstallPath", description: "custom install path"},
+		{name: "TF_PRODUCT", param: "Product", description: "product"},
+		{name: "TF_VERSION", param: "Version", description: "version"},
 	}
-	if envVar := os.Getenv("TF_DEFAULT_VERSION"); envVar != "" {
-		params.DefaultVersion = envVar
-		logger.Debugf("Using default version from environment variable \"TF_DEFAULT_VERSION\": %q", envVar)
-	}
-	if envVar := os.Getenv("TF_PRODUCT"); envVar != "" {
-		params.Product = envVar
-		logger.Debugf("Using product from environment variable \"TF_PRODUCT\": %q", envVar)
-	}
-	if envVar := os.Getenv("TF_BINARY_PATH"); envVar != "" {
-		params.CustomBinaryPath = envVar
-		logger.Debugf("Using custom binary path from environment variable \"TF_BINARY_PATH\": %q", envVar)
-	}
-	if envVar := os.Getenv("TF_INSTALL_PATH"); envVar != "" {
-		params.InstallPath = envVar
-		logger.Debugf("Using custom install path from environment variable \"TF_INSTALL_PATH\": %q", envVar)
+
+	reflectedParams := reflect.ValueOf(&params)
+	for _, envVar := range envVars {
+		name := envVar.name
+		param := envVar.param
+		description := envVar.description
+
+		if len(name) == 0 {
+			logger.Errorf("Internal error: environment variable name is empty for parameter %q mapping, skipping assignment", param)
+			continue
+		}
+		if len(param) == 0 {
+			logger.Errorf("Internal error: parameter name is empty for environment variable %q mapping, skipping assignment", name)
+			continue
+		}
+		if len(description) == 0 {
+			description = param
+		}
+
+		f := reflect.Indirect(reflectedParams).FieldByName(param)
+		if f.Kind() != reflect.String {
+			logger.Warnf("Parameter %q is not a string, skipping assignment from environment variable %q", param, name)
+			continue
+		}
+
+		if envVarValue := os.Getenv(name); envVarValue != "" {
+			logger.Debugf("Using %s from environment variable %q: %q", description, name, envVarValue)
+			if !f.CanSet() {
+				logger.Warnf("Parameter %q cannot be set, skipping assignment from environment variable %q", param, name)
+			}
+			f.SetString(envVarValue)
+		}
 	}
 	return params
 }
