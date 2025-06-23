b42b6cdec86a02d7f94f2dfc7c8881e485470ea3
George L. Yermulnik <yz@yz.kiev.ua>
2025-06-24 00:51:48 +0300

    [`lib/param_parsing/toml_test.go`] Optimize and add test for NoColor/ForceColor

--- lib/param_parsing/toml_test.go
+++ lib/param_parsing/toml_test.go
@@ -8,98 +8,121 @@ import (
 	"github.com/warrensbox/terraform-switcher/lib"
 )
 
-func prepare() Params {
+func prepare(tomlPath string) (Params, error) {
+	if tomlPath == "" {
+		tomlPath = "../../test-data/integration-tests/test_tfswitchtoml"
+	}
+
 	var params Params
-	params.TomlDir = "../../test-data/integration-tests/test_tfswitchtoml"
+	params.TomlDir = tomlPath
 	logger = lib.InitLogger("DEBUG")
-	return params
+	return getParamsTOML(params)
 }
 
 func TestGetParamsTOML_BinaryPath(t *testing.T) {
 	expected := "/usr/local/bin/terraform_from_toml"
-	os.Setenv("BIN_DIR_FROM_TOML", "/usr/local/bin")
-	params := prepare()
-	params, err := getParamsTOML(params)
+	os.Setenv("BIN_DIR_FROM_TOML", "/usr/local/bin") // TOML value utilizes env var expansion
+	params, err := prepare("../../test-data/integration-tests/test_tfswitchtoml")
 	if err != nil {
-		t.Fatalf("Got error '%s'", err)
+		t.Fatalf("Got error: %v", err)
 	}
-	if params.CustomBinaryPath != expected {
-		t.Errorf("BinaryPath not matching. Got %v, expected %v", params.CustomBinaryPath, expected)
+	actual := params.CustomBinaryPath
+	if actual != expected {
+		t.Errorf("%s not matching. Got %q, expected %q", "CustomBinaryPath", actual, expected)
 	}
 	os.Unsetenv("BIN_DIR_FROM_TOML")
 }
 
 func TestGetParamsTOML_InstallPath(t *testing.T) {
 	expected := "/tmp"
-	os.Setenv("INSTALL_DIR_FROM_TOML", "/tmp")
-	params := prepare()
-	params, err := getParamsTOML(params)
+	os.Setenv("INSTALL_DIR_FROM_TOML", "/tmp") // TOML value utilizes env var expansion
+	params, err := prepare("../../test-data/integration-tests/test_tfswitchtoml")
 	if err != nil {
-		t.Fatalf("Got error %v", err)
+		t.Fatalf("Got error: %v", err)
 	}
-	if params.InstallPath != expected {
-		t.Errorf("InstallPath not matching. Got %q, expected %q", params.InstallPath, expected)
+	actual := params.InstallPath
+	if actual != expected {
+		t.Errorf("%s not matching. Got %q, expected %q", "InstallPath", actual, expected)
 	}
 	os.Unsetenv("INSTALL_DIR_FROM_TOML")
 }
 
 func TestGetParamsTOML_Version(t *testing.T) {
 	expected := "1.6.2"
-	params := prepare()
-	params, err := getParamsTOML(params)
+	params, err := prepare("../../test-data/integration-tests/test_tfswitchtoml")
 	if err != nil {
-		t.Fatalf("Got error '%s'", err)
+		t.Fatalf("Got error: %v", err)
 	}
-	if params.Version != expected {
-		t.Errorf("Version not matching. Got %v, expected %v", params.Version, expected)
+	actual := params.Version
+	if actual != expected {
+		t.Errorf("%s not matching. Got %q, expected %q", "Version", actual, expected)
 	}
 }
 
 func TestGetParamsTOML_Default_Version(t *testing.T) {
 	expected := "1.5.4"
-	params := prepare()
-	params, err := getParamsTOML(params)
+	params, err := prepare("../../test-data/integration-tests/test_tfswitchtoml")
 	if err != nil {
-		t.Fatalf("Got error '%s'", err)
+		t.Fatalf("Got error: %v", err)
 	}
-	if params.DefaultVersion != expected {
-		t.Errorf("Version not matching. Got %v, expected %v", params.DefaultVersion, expected)
+	actual := params.DefaultVersion
+	if actual != expected {
+		t.Errorf("%s not matching. Got %q, expected %q", "DefaultVersion", actual, expected)
 	}
 }
 
 func TestGetParamsTOML_log_level(t *testing.T) {
 	expected := "NOTICE"
-	params := prepare()
-	params, err := getParamsTOML(params)
+	params, err := prepare("../../test-data/integration-tests/test_tfswitchtoml")
 	if err != nil {
-		t.Fatalf("Got error '%s'", err)
+		t.Fatalf("Got error: %v", err)
 	}
-	if params.LogLevel != expected {
-		t.Errorf("Version not matching. Got %v, expected %v", params.LogLevel, expected)
+	actual := params.LogLevel
+	if actual != expected {
+		t.Errorf("%s not matching. Got %q, expected %q", "LogLevel", actual, expected)
+	}
+}
+
+func TestGetParamsTOML_no_color(t *testing.T) {
+	expected := false
+	params, err := prepare("../../test-data/integration-tests/test_tfswitchtoml")
+	if err != nil {
+		t.Fatalf("Got error: %v", err)
+	}
+	actual := params.NoColor
+	if actual != expected {
+		t.Errorf("%s not matching. Got %v, expected %v", "NoColor", actual, expected)
+	}
+}
+
+func TestGetParamsTOML_force_color(t *testing.T) {
+	expected := false
+	params, err := prepare("../../test-data/integration-tests/test_tfswitchtoml")
+	if err != nil {
+		t.Fatalf("Got error: %v", err)
+	}
+	actual := params.ForceColor
+	if actual != expected {
+		t.Errorf("%s not matching. Got %v, expected %v", "ForceColor", actual, expected)
 	}
 }
 
 func TestGetParamsTOML_no_file(t *testing.T) {
-	var params Params
-	params.TomlDir = "../../test-data/skip-integration-tests/test_no_file"
-	params, err := getParamsTOML(params)
+	params, err := prepare("../../test-data/skip-integration-tests/test_no_file")
 	if err != nil {
-		t.Fatalf("Got error '%s'", err)
+		t.Fatalf("Got error: %v", err)
 	}
 	if params.Version != "" {
-		t.Errorf("Expected empty version string. Got: %v", params.Version)
+		t.Errorf("Expected empty version string. Got: %q", params.Version)
 	}
 }
 
 func TestGetParamsTOML_error_in_file(t *testing.T) {
-	logger = lib.InitLogger("DEBUG")
-	var params Params
-	params.TomlDir = "../../test-data/skip-integration-tests/test_tfswitchtoml_error"
-	params, err := getParamsTOML(params)
+	params, err := prepare("../../test-data/skip-integration-tests/test_tfswitchtoml_error")
 	if err == nil {
-		t.Errorf("Expected error for reading erroneous toml file. Got nil")
+		t.Fatalf("Expected error for reading erroneous TOML file. Got nil")
 	}
 	if params.Version != "" {
-		t.Errorf("Version should be empty")
+		t.Errorf("Expected empty version string. Got: %q", params.Version)
 	}
 }
